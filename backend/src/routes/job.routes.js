fe2832a87623d428308969389d23d9661eee95ee
Jonathan Mejia <jonathanmejia.ar@gmail.com>
2021-04-07 19:30:49 -0300

    refactor: routes order changed

--- backend/src/routes/job.routes.js
+++ backend/src/routes/job.routes.js
@@ -5,10 +5,10 @@ const jobRouter = Router();
 
 jobRouter.get('/completed', jobCtrl.getCompletedJobs);
 jobRouter.put('/complete/:id', jobCtrl.completeJob);
-jobRouter.get('/', jobCtrl.getJobs);
 jobRouter.get('/:id', jobCtrl.getJob);
-jobRouter.post('/', jobCtrl.createJob);
+jobRouter.get('/', jobCtrl.getJobs);
 jobRouter.put('/:id', jobCtrl.updateJob);
+jobRouter.post('/', jobCtrl.createJob);
 jobRouter.delete('/:id', jobCtrl.deleteJob);
 
 export default jobRouter;
