0a50de0845ec0ed3164060f3c71ecda5f7121b5a
Jonathan Mejia <jonathanmejia.ar@gmail.com>
2021-04-07 13:52:16 -0300

    feat: swagger integration

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,14 +4,19 @@ import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import morgan from 'morgan';
+import swaggerUi from 'swagger-ui-express';
 import indexRouter from './routes/index.routes';
 import mongoConnect from './database';
 import config from './config';
 import passport from 'passport';
 import passportMiddleware from './middlewares/passport';
+import swaggerDoc from './docs/swagger.json';
+
 
 const app = express();
 const PORT = config.PORT || 5000;
+
+app.use('/v1/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 //middlewares
 app.use(express.json());
