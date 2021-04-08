8cb38a8d7014fb23c7f845f36a039149e87e9ffc
Jonathan Mejia <jonathanmejia.ar@gmail.com>
2021-04-07 23:52:31 -0300

    refactor: config log level added

--- backend/src/config/index.js
+++ backend/src/config/index.js
@@ -5,20 +5,6 @@ if (!envFound) {
     throw new Error('.env file not found');
 };
 
-
-/*
-export default {
-    MONGODB_URI: process.env.MONGODB_URI,
-    PORT: process.env.PORT,
-    SECRET: process.env.SECRET,
-    NODE_ENV: process.env.NODE_ENV,
-    MONGODB_URI_TESTING: process.env.MONGODB_URI_TESTING,
-    log: {
-        level: process.env.LOG_LEVEL
-    }
-};
-*/
-
 export default {
     port: process.env.PORT,
     api: {
